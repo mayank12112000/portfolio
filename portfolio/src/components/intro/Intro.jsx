efc0cd7dfaf24ef0eb3852913f1049b923678e28
Mayank  Keshari <mayank.keshari.93@gmail.com>
2024-05-25 19:08:45 +0530

    snap scroll addition of sections

---
+++ portfolio/src/components/intro/Intro.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./intro.css"
+export default function Intro() {
+  return (
+    <div className='intro'>
+      intro
+    </div>
+  )
+}
---
+++ portfolio/src/components/work/Work.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './work.css'
+export default function Work() {
+  return (
+    <div className='work'>
+      work
+    </div>
+  )
+}
